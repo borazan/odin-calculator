e219b1319891bd5f036152dd03080d4b0b83cf8f
borazan <boratuncel@gmail.com>
2022-03-14 18:01:07 +0100

    add main functions

---
+++ script.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return Math.round((a / b) * 10) / 10; //Round to 1 floating point
+}
+
+function operate(a, operator, b) {
+  switch (operator) {
+    case "add":
+      return add(a, b);
+    case "subtract":
+      return subtract(a, b);
+    case "multiply":
+      return multiply(a, b);
+    case "divide":
+      return divide(a, b);
+    default:
+      return null;
+  }
+}
